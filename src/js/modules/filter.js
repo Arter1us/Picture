58c5ee84cd6135b79884a9a124c4686c45e37f4a
Arterius <zaghidullinr@bk.ru>
2021-10-03 18:22:02 +0500

    Калькулятор расчёт общей стоимости, фильтрация элементов, замена изображения при наведении

---
+++ src/js/modules/filter.js
@@ -0,0 +1,24 @@
+const filter = () => {
+    const menu = document.querySelector('.portfolio-menu'),
+        items = menu.querySelectorAll('li'),
+        btnAll = menu.querySelector('.all'),
+        btnLovers = menu.querySelector('.lovers'),
+        btnChef = menu.querySelector('.chef'),
+        btnGirl = menu.querySelector('.girl'),
+        btnGuy = menu.querySelector('.guy'),
+        btnGrandmother = menu.querySelector('.grandmother'),
+        btnGranddad = menu.querySelector('.granddad'),
+        wrapper = document.querySelector('.portfolio-wrapper'),
+        markAll = wrapper.querySelectorAll('.all'),
+        markGirl = wrapper.querySelectorAll('.girl'),
+        markLovers = wrapper.querySelectorAll('.lovers'),
+        markChef = wrapper.querySelectorAll('.chef'),
+        markGuy = wrapper.querySelectorAll('.guy'),
+        no = document.querySelector('.portfolio-no');
+
+    const typeFilter = (markType) => {
+
+    };
+};
+
+export default filter;
